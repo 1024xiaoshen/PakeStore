fc08e3edcc1d023cf94611bbe54fd84c4356dcee
1024小神 <15670339118@163.com>
2025-10-09 19:00:05 +0800

    Enhance login page to display success message upon user authentication

--- src/app/(blank)/login/page.tsx
+++ src/app/(blank)/login/page.tsx
@@ -1,20 +1,23 @@
 'use client'
 import useAppStore from '@/store/app'
 import useUserStore from '@/store/user'
+import { useState } from 'react'
 
 export default function Login() {
     const userStore: any = useUserStore()
     const appStore: any = useAppStore()
+    const [active, setActive] = useState(false)
 
     const handleSubmit = (e: React.MouseEvent<HTMLButtonElement>) => {
         e.preventDefault()
         userStore.setUser(true)
         appStore.setApp(true)
+        setActive(true)
     }
 
     return (
         <div>
-            <h1>登录</h1>
+            <h1>{active ? '登录成功' : '登录'}</h1>
             <form>
                 <input type="text" placeholder="用户名" />
                 <input type="password" placeholder="密码" />
