2fe6d80b4f6474c6544f0c4c07711a7aebd75064
1024小神 <15670339118@163.com>
2025-10-09 14:42:32 +0800

    Update next.config.ts with additional configuration options and standardize formatting. Modify package.json for consistent indentation.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+    /* config options here */
+    output: 'export',
+    distDir: 'dist',
+    trailingSlash: true,
+    images: {
+        unoptimized: false,
+    },
+}
 
-export default nextConfig;
+export default nextConfig
