e5bc93f475f89302fa5a6d7d0a54f5e05a107c1a
1024小神 <15670339118@163.com>
2025-10-12 18:13:56 +0800

    Refactor Home page layout to improve carousel display; adjust image sizes and spacing for better responsiveness and visual appeal.

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -6,54 +6,53 @@ import {
     CarouselNext,
     CarouselPrevious,
 } from '@/components/ui/carousel'
+import Image from 'next/image'
 import Autoplay from 'embla-carousel-autoplay'
 
 export default function Home() {
     return (
         <div className="flex gap-4 mt-4 h-100">
             {/* left */}
-            <div className="w-2/3 h-full">
-                <div className="w-full h-full">
-                    {/* Carousel */}
-                    <Carousel
-                        plugins={[
-                            Autoplay({
-                                delay: 2000,
-                            }),
-                        ]}
-                        opts={{ loop: true }}
-                        className="w-full"
-                    >
-                        <CarouselContent>
-                            <CarouselItem>
-                                <img
-                                    src="https://pakeplus.com/assets/config1.B4u9PEWG.webp"
-                                    alt="Carousel 1"
-                                    className="w-full h-full"
-                                />
-                            </CarouselItem>
-                            <CarouselItem>
-                                <img
-                                    src="https://pakeplus.com/assets/phone1.Bg-YGh6e.webp"
-                                    alt="Carousel 2"
-                                    className="w-full h-full"
-                                />
-                            </CarouselItem>
-                            <CarouselItem>
-                                <img
-                                    src="https://pakeplus.com/assets/creat1.BYkNwqDR.webp"
-                                    alt="Carousel 3"
-                                    className="w-full h-full"
-                                />
-                            </CarouselItem>
-                        </CarouselContent>
-                    </Carousel>
-                </div>
+            <div className="w-3/5">
+                {/* Carousel */}
+                <Carousel
+                    plugins={[
+                        Autoplay({
+                            delay: 3000,
+                        }),
+                    ]}
+                    opts={{ loop: true }}
+                    className="w-full h-full"
+                >
+                    <CarouselContent className="h-full">
+                        <CarouselItem>
+                            <img
+                                src="https://pakeplus.com/assets/config1.B4u9PEWG.webp"
+                                alt="Carousel 1"
+                                className="w-full h-full"
+                            />
+                        </CarouselItem>
+                        <CarouselItem>
+                            <img
+                                src="https://pakeplus.com/assets/phone1.Bg-YGh6e.webp"
+                                alt="Carousel 2"
+                                className="w-full h-full"
+                            />
+                        </CarouselItem>
+                        <CarouselItem>
+                            <img
+                                src="https://pakeplus.com/assets/creat1.BYkNwqDR.webp"
+                                alt="Carousel 3"
+                                className="w-full h-full"
+                            />
+                        </CarouselItem>
+                    </CarouselContent>
+                </Carousel>
             </div>
             {/* right */}
-            <div className="w-1/3 flex flex-col gap-4">
+            <div className="w-2/5 flex flex-col">
                 {/* top */}
-                <div className="w-full h-3/5 bg-green-500">
+                <div className="w-full h-3/5 pb-4">
                     <img
                         src="https://pakeplus.com/assets/config1.B4u9PEWG.webp"
                         alt="Carousel 1"
@@ -61,16 +60,16 @@ export default function Home() {
                     />
                 </div>
                 {/* bottom */}
-                <div className="w-full h-2/5 flex gap-4">
+                <div className="w-full h-2/5 flex">
                     <img
                         src="https://pakeplus.com/assets/config1.B4u9PEWG.webp"
                         alt="Carousel 1"
-                        className="h-full"
+                        className="w-1/2 pr-2"
                     />
                     <img
                         src="https://pakeplus.com/assets/config1.B4u9PEWG.webp"
                         alt="Carousel 1"
-                        className="h-full"
+                        className="w-1/2 pl-2"
                     />
                 </div>
             </div>
