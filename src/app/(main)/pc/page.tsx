708b16b0f1da16c5fe081ecf4324b25685fe28e5
1024小神 <15670339118@163.com>
2025-10-15 22:39:20 +0800

    Add pagination functionality to PC component; dynamically generate page numbers based on pageNum for improved navigation and user experience.

--- src/app/(main)/pc/page.tsx
+++ src/app/(main)/pc/page.tsx
@@ -10,6 +10,9 @@ import {
 } from '@/components/ui/pagination'
 
 export default function PC() {
+    const pageNum = 10
+    const pageSize = 10
+
     return (
         <>
             <div className="container mx-auto px-4 md:px-6 flex gap-4">
@@ -37,12 +40,15 @@ export default function PC() {
                                 <PaginationItem>
                                     <PaginationPrevious href="#" />
                                 </PaginationItem>
-                                <PaginationItem>
-                                    <PaginationLink href="#">1</PaginationLink>
-                                </PaginationItem>
-                                <PaginationItem>
-                                    <PaginationEllipsis />
-                                </PaginationItem>
+                                {Array.from({ length: pageNum }).map(
+                                    (_, index) => (
+                                        <PaginationItem key={index}>
+                                            <PaginationLink href={`#`}>
+                                                {index + 1}
+                                            </PaginationLink>
+                                        </PaginationItem>
+                                    )
+                                )}
                                 <PaginationItem>
                                     <PaginationNext href="#" />
                                 </PaginationItem>
